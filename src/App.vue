face42f0ba27f9b6468610bdd78999c9b7a5709e
Luke Lawton <llawton25@student.dimanregional.org>
2023-02-01 22:54:42 -0500

    began production, navbar

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+nav {
+  width: 100%;
+  height: 6vh;
+  font-size: 12px;
+  text-align: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  gap: 3rem;
+  font-size: 2rem;
+}
+
+nav a {
+  padding: 0 0.5rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a:hover {
+  scale: 1.2;
+  background-color: transparent;
+}
+
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="title">Hello, this is for web development tools</h1>
+    <h2>There's going to be all sorts of web development tools here like, color pickers, font pickers, file type to file type converters, and many many more</h2>
+    <p>I don't intend on working on this very often, but whenever I find inspiration I'll make an update</p>
+  </main>
+</template>
+
+<style>
+  main {
+    padding-top: 5%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .title {
+    color: var(--color-text);
+    text-align: center;
+  }
+</style>
